73e678b8028866478c0cf7261a89238ae2614b18
Dozzer51 <z-fehe97@gmx.at>
2025-05-25 18:54:23 +0200

    added colourcoloum at 80 charsw
    Signed-off-by: Dozzer51 <z-fehe97@gmx.at>

--- nvim/lua/config/settings.lua
+++ nvim/lua/config/settings.lua
@@ -1,7 +1,8 @@
-vim.wo.nu = true            --show linenumber
-vim.wo.rnu = true           --show realtive linenumber
-vim.wo.cursorline = true    --highlight cursorline
-vim.wo.so = 15              --try to show 15 line above/under cursorline
+vim.wo.nu = true    --show linenumber
+vim.wo.rnu = true   --show realtive linenumber
+vim.wo.cul = true   --highlight cursorline
+vim.wo.so = 15      --try to show 15 line above/under cursorline
+vim.wo.cc = "80"    --show colourcoloum at 80
 
 --Set tabs to 4 space
 vim.o.et = true
